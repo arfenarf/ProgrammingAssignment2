cc23d3740ae07124116f0748780ea678e474bcc8
Kate Weber <kate@kateweber.com>
2015-04-20 18:49:04 -0400

    Submitted Version
    File contains two functions prepared for Assignment 2 of Programming in
    R.  First submitted version after development and unit testing.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This pair of functions is adapted from sample material
+## provided in the introduction to Programming Assignment 2
+## for Programming in R.
 
-## Write a short comment describing this function
+## This first function establishes a vector loaded with functions
+## that can be applied in the cacheSolve() function to 
+## set and display cached values.  The <<- operators in the 
+## set() and setinv() functions set values in the scope of the calling
+## function cacheSolve().
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        mx <- NULL
+                set <- function(y) {
+                        x <<- y
+                        mx <<- NULL
+                }
+                get <- function() x
+                setinv <- function(inv) mx <<- inv
+                getinv <- function() mx
+                list(set = set, get = get,
+                     setinv = setinv,
+                     getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This second function is the outer of the pair and, once 
+## the sub-functions are built with makeCacheMatrix, calls them
+## to recall and then store cached versions of the inverted matrix.
+## Lines 35-37 check to see whether this inversion has already
+## been calculated
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        mx <- x$getinv()
+        if(!is.null(mx)) {
+                message("getting cached data")
+                return(mx)
+        }
+        data <- x$get()
+        mx <- solve(data, ...)
+        x$setinv(mx)
+        mx
 }
